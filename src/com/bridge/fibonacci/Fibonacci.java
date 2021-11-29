96c4b5b02cfbd9b760102b4c4beee32d860df363
Navneet <navn0306@gmail.com>
2021-11-29 15:09:22 +0530

    ADD: Added the welcome message for Program

---
+++ src/com/bridge/fibonacci/Fibonacci.java
@@ -0,0 +1,8 @@
+package com.bridge.fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome this program prints the Fibonacci series of a given input");
+    }
+}
